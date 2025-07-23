86759d9e2176f7daacd789fe61822ef3a0286638
RSCEY <89863576+rscey@users.noreply.github.com>
2025-07-23 16:16:28 +0300

    Add files via upload

---
+++ proxy_checker.py
@@ -0,0 +1,59 @@
+import requests
+from colorama import Fore
+
+list = open("proxy_list.txt","r")
+proxies = list.readlines()
+
+online_proxies = []
+
+print(Fore.MAGENTA + """
+      
+'||''|.  '||''|.    ..|''||   '||' '|' '||' '|' 
+ ||   ||  ||   ||  .|'    ||    || |     || |   
+ ||...|'  ||''|'   ||      ||    ||       ||    
+ ||       ||   |.  '|.     ||   | ||      ||    
+.||.     .||.  '|'  ''|...|'  .|   ||.   .||.
+      
+      ..|'''.| '||'  '||' '||''''|    ..|'''.| '||'  |'  '||''''|  '||''|.   
+    .|'     '   ||    ||   ||  .    .|'     '   || .'     ||  .     ||   ||  
+    ||          ||''''||   ||''|    ||          ||'|.     ||''|     ||''|'   
+    '|.      .  ||    ||   ||       '|.      .  ||  ||    ||        ||   |.  
+     ''|....'  .||.  .||. .||.....|  ''|....'  .||.  ||. .||.....| .||.  '|' 
+      
+
+                                                    ~by RSCEY
+      
+""")
+
+for line in proxies:
+
+    try:
+        control = requests.get(
+             "http://ipinfo.io/json",
+             proxies={
+                  "http" : line,
+                  "https" : line,
+             }
+        )
+
+    except:
+        continue
+
+    if control.status_code == 200:
+            online_proxies.append(line)
+            print(Fore.GREEN + f"Proxy online: {line.strip()}")
+
+
+with open("online_proxies.txt","w") as online_p:
+     
+     online_p.writelines(online_proxies)
+
+print(Fore.YELLOW + "\nOnline Proxies saved successfully!")
+
+   
+
+
+    
+
+
+                
